cbdf67c0f3b90b67dd91333401b2fd8a83ef08f6
Mob <yadiraies@gmail.com>
2019-06-25 11:57:34 +0800

    注册页面

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import NotFound from "./components/notFound"
 import MovieForm from "./components/movieForm"
 import NavBar from "./components/navBar"
 import LoginForm from "./components/loginForm"
+import RegisterForm from "./components/registerForm"
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
         <NavBar />
         <main className="container">
           <Switch>
+            <Route path="/register" component={RegisterForm} />
             <Route path="/login" component={LoginForm} />
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movies} />
